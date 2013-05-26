13762ce51e00bdbf455159839e643b27bfa3904c
David de Klerk <dawiedotcom@gmail.com>
2013-05-26 14:15:48 +0200

    Move the syntactic environment functions to env.

--- src/scheep/env.clj
+++ src/scheep/env.clj
@@ -2,7 +2,7 @@
   (:gen-class))
 
 ;;;; Provides lexical and syntactic environments for
-;;;; the interpreter and the macro expander, respectivly.
+;;;; the interpreter and the macro expander, respectively.
 
 ;;;; Frames 
 
@@ -33,29 +33,47 @@
 (defn extend-environment [env vars vals] 
   (conj env (make-frame vars vals)))
 
-(defn with-var-in-env [proc var env] 
+(defn with-var-in-env [proc var env not-found-proc] 
   (letfn [(lookup [e]
             (if (the-empty-environment? e)
-                (Exception. (str "Variable " var " is not defined\n"))
+                (not-found-proc var)
                 (let [frame (first-frame e)]
-                  ;(println (str "Looking for " var " in:"))
-                  ;(print-frame frame)
                   (if (frame-defined? frame var)
                       (proc frame var)
                       (recur (enclosing-environment e))))))]
     (lookup env)))
 
+(defn throw-var-not-found [var]
+  (throw (Exception.
+          (str "Variable " var " is not defined."))))
+
+;;;; Lexical environments - for the evaluator
+
 (defn set-variable-value! [env var val]
-  (with-var-in-env #(frame-add-binding! %1 %2 val) var env))
+  (with-var-in-env
+    #(frame-add-binding! %1 %2 val)
+    var
+    env
+    throw-var-not-found))
 (defn lookup-variable-value [exp env]
   ;; Called by eval, only if exp is a variable
-  (try
-    (with-var-in-env frame-get-binding exp env)
-    (catch Exception ex (print-frame (first-frame env))
-           (throw ex))))
+  (with-var-in-env frame-get-binding exp env throw-var-not-found))
 (defn define-variable! [env var val] 
   (if (the-empty-environment? env)
-      (throw (.Exception "Cannot define variables in the-empty-environment"))
+      (throw
+       (.Exception
+        "Cannot define variables in the-empty-environment"))
       (let [frame (first-frame env)]
         (frame-add-binding! frame var val))))
+
+;;;; Syntactic environments - for the macro expander
+
+(defn lookup [id s-env]
+  (with-var-in-env frame-get-binding id s-env identity))
+
+(defn bind [s-env symbols bindings]
+  (extend-environment s-env symbols bindings))
+
+(defn divert [env1 env2]
+  (concat env1 env2))
 
